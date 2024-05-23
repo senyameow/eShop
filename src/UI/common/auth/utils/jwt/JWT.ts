ecc9c18cd13ea679f5cd2e91844765c2583bfa60
senyameow <arseniyxd@gmail.com>
2024-05-23 23:44:09 +0700

    JWT util added

--- src/UI/common/auth/utils/jwt/JWT.ts
+++ src/UI/common/auth/utils/jwt/JWT.ts
@@ -2,17 +2,34 @@ import { injectable } from 'inversify';
 import * as jwt from 'jsonwebtoken'
 import { IJWT } from './IJWT';
 import { CreateTokenRequest } from './requests/createTokenRequest';
+import { JWT_SECRET_KEY } from '../../../config/errors/constants/const';
+import { IncomingHttpHeaders } from 'http';
 
 @injectable()
 export class JWT implements IJWT {
-    private readonly key = 'Bearer'
-    private readonly header = 'Authorization'
     // from https://stackoverflow.com/questions/61802832/regex-to-match-jwt
     private readonly matcher = /^[\w-]+\.[\w-]+\.[\w-]+$/;
 
 
     createToken({ payload, secret, expiresIn }: CreateTokenRequest): string {
-
         return jwt.sign(payload, secret, { expiresIn })
+    }
+
+    decodeToken(token: string): jwt.JwtPayload | null {
+        try {
+            jwt.verify(token, JWT_SECRET_KEY)
+        } catch (error) {
+            return null
+        }
+    }
+
+    getTokenFromHeaders(headers: IncomingHttpHeaders): string | null {
+        const authHeader = headers.authorization
+        if (!authHeader) {
+            return null;
+        }
+        const matches = authHeader.match(this.matcher);
+
+        return matches && matches[2]
     }
 }
