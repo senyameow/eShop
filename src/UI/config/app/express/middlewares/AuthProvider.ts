0e0da446ad5ec94b40c27c76b98b6c60f7a75b7a
senyameow <arseniyxd@gmail.com>
2024-05-24 22:58:17 +0700

    completed AuthProvider

--- src/UI/config/app/express/middlewares/AuthProvider.ts
+++ src/UI/config/app/express/middlewares/AuthProvider.ts
@@ -8,6 +8,7 @@ import { DOMAIN_REPOSITORIES_SYMBOLS, DOMAIN_SERVICES_SYMBOLS } from "../../../.
 import { JWTPayload } from "../auth/jwt/JWTPayload";
 import { IUserService } from "../../../../../core/applicationServices/User/IUserService";
 import { GetUserByEmailRequest } from "../../../../../core/applicationServices/User/requests/GetUserByEmailRequest";
+import { Principal } from "../auth/models/Principal";
 
 // AuthProvider интерфейс говорит классу, чтонадо имплементировать getUser и вернуть какой-то Principal в промисе
 // при этом getUser это экспресовская мидлвара, поэтому ей место в middlewares в зоне ответственности экспресса (ествественно в слое UI)
@@ -25,26 +26,23 @@ export class AuthProvider implements interfaces.AuthProvider {
     ): Promise<interfaces.Principal> {
         const token = this.jwt.getTokenFromHeaders(req.headers)
 
-        // if (!token) return new Principal -> TODO create new empty Principal 
         // https://www.npmjs.com/package/inversify-express-utils
         const data = this.jwt.decodeToken<JWTPayload>(token)
-        // { email: 'senya@mail.ru', role: 'USER', id: 0, iat: 1716562734 }
-        // могу засерчить юзера по емайлу -> обращаюсь к юзерРепо
+        // { User, iat: 1716562734 }
         if (data === null) {
-            // handle 
-            return undefined
+            return new Principal(null)
         }
         const { user: userData } = data
-        // TS ругается, т.к. не знает ничего про email, для этого я делал дженерик, щас надо дать дженерик, где будет email 
-        // а вообще я должен обращаться к app сервису, а не репозиторию.. че я делаю
         try {
             const user = await this.userService.getUserByEmail(new GetUserByEmailRequest(userData.Contact.email))
 
-            if (!user) return undefined
+            if (!user) return new Principal(null)
+
+            return new Principal(user)
 
         } catch (error) {
             next(error)
-            return undefined // вернуть Principal todo
+            return new Principal(null)
         }
     }
 
